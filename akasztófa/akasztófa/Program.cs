14a273aca88dc3959a4d27c8a4ca8b319a9d8bd6
Sanya52 <70943490+Sanya52@users.noreply.github.com>
2021-02-01 10:55:26 +0100

    Add files via upload

---
+++ akasztófa/akasztófa/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace akasztófa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("szavak.txt",FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            List<String> lista = new List<string>();
+
+            Random r = new Random();
+
+            while (!sr.EndOfStream)
+            {
+             lista.Add(sr.ReadLine());
+            }
+            int a=r.Next(0, lista.Count);
+            string szo = "";
+            string allas = "";
+            string ideiglenes = "";
+            for (int i = 0; i < lista.Count; i++)
+            {
+                szo = lista[a];
+                break;
+            }
+            for (int i = 0; i < szo.Length; i++)
+            {
+                allas += "_";
+            }
+
+            bool ingame = true;
+            Console.WriteLine(allas);
+            int proba = 0;
+            while (ingame==true)
+            {
+                Console.WriteLine("Választott betű: ");
+                char betu = Console.ReadKey().KeyChar;
+                proba++;
+                for (int i = 0; i < szo.Length; i++)
+                {
+                    if (betu==szo[i])
+                    {
+                        ideiglenes += szo[i];
+                    }
+                    else
+                    {
+                        ideiglenes += allas[i];
+                    }
+                }
+                allas = ideiglenes;
+                ideiglenes = "";
+                if (allas.Contains("_"))
+                {
+                   Console.WriteLine("\n"+allas+ "(próbálkozások száma: {0})",proba);
+                    int gf = szo.Length+5;
+                    if (proba>=gf)
+                    {
+                        Console.WriteLine("\nVesztettél! Elég ramatyul játszol! Kéne egy kis gyakorlás még!");
+                        ingame = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nNyertél! A szó a/az {0} volt!", szo);
+                    ingame = false;
+                }
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
